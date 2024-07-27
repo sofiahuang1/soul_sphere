6c8f39ab7936f5af2fa09ec961c0714b5ce07654
sofiahuang1 <sofiahuang5678@gmail.com>
2024-07-27 20:23:45 +0200

    nav bar complete

--- lib/app/constants/app_colors.dart
+++ lib/app/constants/app_colors.dart
@@ -5,14 +5,14 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const LinearGradient primaryGradient = LinearGradient(
     colors: [
-      Color.fromARGB(255, 135, 209, 182),
-      Color.fromARGB(255, 187, 127, 219),
+      Color.fromARGB(255, 70, 248, 183),
+      Color.fromARGB(255, 185, 72, 246),
     ],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
     transform: GradientRotation(pi / 5),
   );
-  static const Color purple = Color.fromARGB(255, 154, 134, 172);
+  static const Color purple = Color.fromARGB(255, 164, 100, 221);
   static const Color white = Colors.white;
   static Color opacityWhite = Colors.white.withOpacity(0.7);
 }
