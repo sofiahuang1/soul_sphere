a5fc997a71ed09177a5e15a895f361556d2a8638
sofiahuang1 <sofiahuang5678@gmail.com>
2024-08-23 11:36:16 +0200

    refactor code

--- lib/presentation/feature/chat/all_chat_screen.dart
+++ lib/presentation/feature/chat/all_chat_screen.dart
@@ -1,11 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 import 'package:soul_sphere/app/constants/app_constants.dart';
-import 'package:soul_sphere/app/router/app_paths.dart';
-import 'package:soul_sphere/domain/model/chat_message.dart';
-import 'package:soul_sphere/presentation/feature/chat/widget/chat_item.dart';
+import 'package:soul_sphere/presentation/feature/chat/widget/chat_list_view.dart';
 import 'package:soul_sphere/presentation/feature/one_one_chat/bloc/current_user/current_user_bloc.dart';
 import 'package:soul_sphere/presentation/feature/one_one_chat/bloc/current_user/current_user_event.dart';
 import 'package:soul_sphere/presentation/feature/one_one_chat/bloc/current_user/current_user_state.dart';
@@ -45,32 +42,8 @@ class AllChatScreen extends StatelessWidget {
                     return const Center(child: Text('No chats yet'));
                   }
 
-                  return ListView.builder(
-                    itemCount: chatDocs.length,
-                    itemBuilder: (context, index) {
-                      final chatData =
-                          chatDocs[index].data() as Map<String, dynamic>;
-                      final chatMessage = ChatMessage.fromFirestore(
-                          chatData, chatDocs[index].id);
-
-                      return ChatItem(
-                        chatMessage: chatMessage,
-                        currentUserId: currentUserId,
-                        onTap: () {
-                          context.push(
-                            AppPaths.chatScreenPath,
-                            extra: {
-                              'currentUserId': currentUserId,
-                              'randomUser':
-                                  chatMessage.receiverId == currentUserId
-                                      ? chatMessage.senderId
-                                      : chatMessage.receiverId,
-                            },
-                          );
-                        },
-                      );
-                    },
-                  );
+                  return ChatListView(
+                      chatDocs: chatDocs, currentUserId: currentUserId);
                 },
               );
             } else if (state is UserIdError) {
--- lib/presentation/feature/chat/widget/chat_item.dart
+++ lib/presentation/feature/chat/widget/chat_item.dart
@@ -1,68 +1,42 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:soul_sphere/app/constants/app_colors.dart';
 import 'package:soul_sphere/app/utils/utils.dart';
-import 'package:soul_sphere/data/model/user_model.dart';
 import 'package:soul_sphere/domain/entities/user_entity.dart';
 import 'package:soul_sphere/domain/model/chat_message.dart';
 
 class ChatItem extends StatelessWidget {
   final ChatMessage chatMessage;
-  final String currentUserId;
+  final UserEntity user;
   final VoidCallback onTap;
 
   const ChatItem({
     required this.chatMessage,
-    required this.currentUserId,
+    required this.user,
     required this.onTap,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<DocumentSnapshot>(
-      future: FirebaseFirestore.instance
-          .collection('users')
-          .doc(chatMessage.receiverId == currentUserId
-              ? chatMessage.senderId
-              : chatMessage.receiverId)
-          .get(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return const ListTile(
-            title: Text('Loading...'),
-          );
-        }
-
-        final userModel = UserModel.fromFirestore(snapshot.data!);
-        final user = UserEntity(
-          id: userModel.id,
-          email: userModel.email,
-          avatar: userModel.avatar,
-          bio: userModel.bio,
-          createdAt: userModel.createdAt,
-          followersCount: userModel.followersCount,
-          followingCount: userModel.followingCount,
-          postCount: userModel.postCount,
-        );
-
-        return ListTile(
-          leading: CircleAvatar(
-            backgroundImage: AssetImage(user.avatar),
-          ),
-          title: Text(user.email),
-          subtitle: Text(
-            chatMessage.text,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-          ),
-          trailing: Text(
-            Utils.formatTimestamp(chatMessage.timestamp),
-            style: const TextStyle(color: AppColors.lightGrey),
-          ),
-          onTap: onTap,
-        );
-      },
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: AssetImage(user.avatar),
+      ),
+      title: Text(
+        user.email.length > 20
+            ? '${user.email.substring(0, 7)}...'
+            : user.email,
+      ),
+      subtitle: Text(
+        chatMessage.text,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      ),
+      trailing: Text(
+        Utils.formatTimestamp(chatMessage.timestamp),
+        style: const TextStyle(color: AppColors.lightGrey),
+      ),
+      onTap: onTap,
     );
   }
 }
